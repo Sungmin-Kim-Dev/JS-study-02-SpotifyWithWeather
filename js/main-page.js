81fd2da35052c3b576ee91ccc0c063961e0b3a3a
Sungmin-Kim-Dev <dreamerk24dev@gmail.com>
2024-07-21 00:13:53 +0900

    API new release album

--- js/main-page.js
+++ js/main-page.js
@@ -1,14 +1,72 @@
-const ClientID = `411944574c3745f49a0ae819f629170b`;
-const ClientSecret = `20eb343e234c4ec7ac730215cab938cc`;
-const artistURL = `https://api.spotify.com/v1/artists/`;
+const ClientIDSKim = config.clientID;
+const ClientSecretSKim = config.clientSecret;
+const artistAPI = `https://api.spotify.com/v1/artists/`;
 
-const mainTopArtistLine = async () => {
-  let url = new URL(artistURL + '0TnOYISbd1XYRBk9myaseg');
-  const response = await fetch(url);
+const getTokenSKim = async () => {
+  const encoded_Credentials = btoa(`${ClientIDSKim}:${ClientSecretSKim}`);
+  const response = await fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    body: new URLSearchParams({
+      grant_type: 'client_credentials',
+    }),
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: `Basic ${encoded_Credentials}`,
+    },
+  });
+  // console.log(response);
   const data = await response.json();
-  console.log('URL:', url);
-  console.log('response:', response);
-  console.log('data:', data);
+  // console.log(data.access_token);
+  return data.access_token;
 };
 
-// mainTopArtistLine();
+// https://open.spotify.com/section/0JQ5DAnM3wGh0gz1MXnu3C
+let popularArtistsURL = `https://api.spotify.com/v1/top/artists`;
+// https://api.spotify.com/v1/browse/new-releases
+let newReleaseURL = `https://api.spotify.com/v1/browse/new-releases`;
+
+const fetchNewReleases = async (url, token) => {
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch ${url} data`);
+  }
+  const data = await response.json();
+  console.log(data);
+  return data;
+};
+
+const callAlbumLine = async (url, HTML_ID) => {
+  url = url + `?limit=9`
+  const token = await getTokenSKim();
+  let itemList = await fetchNewReleases(url, token);
+  let albumList = itemList.albums.items;
+  renderCardLine(albumList, HTML_ID);
+};
+
+callAlbumLine(newReleaseURL, 'new-release-line');
+
+const renderCardLine = (list, HTML_ID) => {
+  let cardItemHTML = list.map((album) => {
+    return `<div class="contents-card">
+      <div class="card-img-box position-relative">
+        <div class="card-play-btn"></div>
+        <div class="card-img">
+          <img src="${album.images[0].url}" alt="">
+        </div>
+      </div>
+      <div class="card-text">
+        <p class="card-title">${album.name}</p>
+        <p class="card-subtitle card-subtitle-artist">${album.artists[0].name}</p>
+      </div>
+    </div>`;
+  });
+  document.getElementById(HTML_ID).innerHTML = cardItemHTML.join('');
+};
+
+
